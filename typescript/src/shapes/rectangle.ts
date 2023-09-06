d993800b8b1bfe082bb5ad3035581112849283a6
Gabriel <adrastopoulos@gmail.com>
2023-09-06 10:16:17 -0400

    feat: rectangle extend shape

--- typescript/src/shapes/rectangle.ts
+++ typescript/src/shapes/rectangle.ts
@@ -1,14 +1,12 @@
-interface Rectangle {
+interface Rectangle extends Shape {
     width: number,
     height: number,
-    computeArea: () => number
 }
 
 function newRectangle(width: number, height: number): Rectangle {
     return {
         width,
         height,
-
         computeArea: function (): number {
             return width * height
         }
